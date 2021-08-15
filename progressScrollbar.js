fc273e378fa62f54923bcdd1cb860196d976ef3c
juhi1209 <juhi12.mail@gmail.com>
2021-08-15 19:10:05 -0400

    progress scroll bar

---
+++ progressScrollbar.js
@@ -0,0 +1,21 @@
+import { fromEvent } from 'rxjs';
+import { map } from 'rxjs/operators'
+//helper
+function calculateScrollPercent(element) {
+    const { scrollTop, scrollHeight, clientHeight} = element;
+    return (scrollTop/(scrollHeight - clientHeight))*100;
+}
+
+const progressBar = document.querySelector('.progress-bar');
+
+const scroll$ = fromEvent(document, 'scroll');
+const progress$ = scroll$.pipe(
+    map(({target}) => calculateScrollPercent(target.scrollingElement))
+);
+
+
+progress$.subscribe(percent => {
+    progressBar.style.width = `${percent}%`
+});
+
+
